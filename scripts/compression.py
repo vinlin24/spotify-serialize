9aa4913df5edd9d151e0b8678f7dc4a4e4132fff
Vincent Lin <vinlin24@outlook.com>
2023-02-01 16:02:02 -0800

    Make fixes and generalize input to text, not JSON

--- scripts/compression.py
+++ scripts/compression.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 """Simple zlib compression/decompression script.
 
-USAGE: {c,d,compress,decompress} FROM_PATH TO_PATH
+USAGE: ./compression.py {c,d,compress,decompress} FROM_PATH TO_PATH
 """
 
-import json
 import sys
 import zlib
 from pathlib import Path
@@ -13,27 +12,25 @@ from typing import Callable, NoReturn
 
 __author__ = "Vincent Lin"
 
-USAGE_LINE = "{c,d,compress,decompress} FROM_PATH TO_PATH"
+USAGE_LINE = "./compression.py {c,d,compress,decompress} FROM_PATH TO_PATH"
 
 
 def compress(src_path: Path, bin_path: Path) -> None:
-    """Compress a JSON file into a binary file."""
+    """Compress a text file into a binary file."""
     with src_path.open("rt", encoding="utf-8") as fp:
-        json_dict = json.load(fp)
-    json_string = json.dumps(json_dict)
-    payload = zlib.compress(json_string.encode("utf-8"))
+        content = fp.read()
+    payload = zlib.compress(content.encode("utf-8"))
     with bin_path.open("wb") as fp:
         fp.write(payload)
 
 
 def decompress(bin_path: Path, src_path: Path) -> None:
-    """Decompress a binary file into a JSON file."""
+    """Decompress a binary file into a text file."""
     with bin_path.open("rb") as fp:
         compressed = fp.read()
-    json_string = zlib.decompress(compressed).decode("utf-8")
-    json_dict = json.loads(json_string)
+    content = zlib.decompress(compressed).decode("utf-8")
     with src_path.open("wt", encoding="utf-8") as fp:
-        json.dump(json_dict, fp)
+        fp.write(content)
 
 
 def exit_with_message(message: str) -> NoReturn:
@@ -49,15 +46,13 @@ def main() -> None:
         _, command, from_path, to_path, *_ = sys.argv
     except ValueError:
         exit_with_message(
-            f"Expected at least 3 arguments, got {len(sys.argv)} instead.")
+            f"Expected at least 3 arguments, got {len(sys.argv)-1} instead.")
 
     from_path = Path(from_path)
     to_path = Path(to_path)
 
-    def execute_command(func: Callable[[Path, Path], None],
-                        from_path: Path,
-                        to_path: Path
-                        ) -> None:
+    def execute_command(func: Callable[[Path, Path], None]) -> None:
+        """Exception handler for executing the command callbacks."""
         try:
             func(from_path, to_path)
         except FileNotFoundError as e:
@@ -65,10 +60,10 @@ def main() -> None:
 
     option = command.lower()[0]
     if option == "c":
-        execute_command(compress, from_path, to_path)
+        execute_command(compress)
         print(f"Compressed {from_path} to {to_path}.")
     elif option == "d":
-        execute_command(decompress, from_path, to_path)
+        execute_command(decompress)
         print(f"Decompressed {from_path} to {to_path}.")
     else:
         exit_with_message("Expected[c]ompress/[d]ecompress as first argument.")
