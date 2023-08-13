52933bd3392e6a5020d6b8611b005beaad8b95fb
Vincent Lin <vinlin24@outlook.com>
2023-08-13 00:18:09 -0700

    Overhaul serialize command to use new schema

--- src/spotify_serialize/commands/serialize.py
+++ src/spotify_serialize/commands/serialize.py
@@ -4,139 +4,157 @@ Implement serializing the user's library into a compressed data format.
 """
 
 import json
-import zlib
-from dataclasses import asdict
-from pathlib import Path
-from typing import BinaryIO, Generator, List, Tuple
+from typing import Any, Dict, Generator, Iterator, List, TextIO, Tuple, Union
 
 import click
 import tekore
-from tekore.model import (FullPlaylist, LocalPlaylistTrack, PlaylistTrack,
-                          SavedTrack, SimplePlaylist)
 
-from ..utils import PlaylistState, SpotifyID, get_client
+from ..client import get_client
+
+# TODO: handle downloading images
+
+JSONData = Dict[str, Any]
+
+User = Union[tekore.model.PublicUser, tekore.model.PrivateUser]
+
+
+def user_model_to_json(user: User) -> JSONData:
+    """
+    Convert a tekore user model to the format of user.schema.json.
+    """
+    return {
+        "id": user.id,
+        "displayName": user.display_name,
+        "numFollowers": user.followers and user.followers.total,
+    }
+
+
+Track = Union[tekore.model.SavedTrack, tekore.model.PlaylistTrack]
+
+
+def track_model_to_json(track: Track) -> JSONData:
+    """
+    Convert a tekore track model to the format of track.model.json.
+    """
+    _track: Union[tekore.model.FullTrack, tekore.model.FullPlaylistTrack]
+    _track = track.track  # type: ignore
+    return {
+        "id": _track.id,
+        "name": _track.name,
+        "artists": [artist.name for artist in _track.artists],
+        "addedAt": track.added_at.isoformat()
+    }
+
+
+Playlist = tekore.model.SimplePlaylist
+
+
+def playlist_model_to_json(playlist: Playlist, spotify: tekore.Spotify
+                           ) -> JSONData:
+    """
+    Convert a tekore playlist model to the format of
+    playlist.schema.json.
+    """
+    # Convert simple playlist to full playlist to access tracks
+    full_playlist: tekore.model.FullPlaylist
+    full_playlist = spotify.playlist(playlist.id)  # type: ignore
+
+    paging = full_playlist.tracks
+    track_iterator: Generator[tekore.model.PlaylistTrack, None, None]
+    track_iterator = spotify.all_items(paging)  # type: ignore
+
+    # NOTE: I don't know why, but for some reason the iterator has to be
+    # iterated through once beforehand for the comprehension below to
+    # work.  Maybe something to do with the click.progressbar()?
+    list(track_iterator)
+
+    return {
+        "id": full_playlist.id,
+        "name": full_playlist.name,
+        "description": full_playlist.description or None,
+        "tracks": [track_model_to_json(track) for track in track_iterator]
+    }
 
 
 class Serializer:
     def __init__(self, spotify: tekore.Spotify) -> None:
         self.spotify = spotify
 
-    def get_library_json(self) -> dict:
-        owned, followed = self._serialize_playlists()
+    def serialize(self) -> JSONData:
+        user_data = self._serialize_profile()
+        saved_songs = self._serialize_saved_songs()
+        owned_playlists, followed_playlists = self._serialize_playlists()
         return {
-            "playlists": {
-                "owned": [asdict(state) for state in owned],
-                "followed": followed,
-            },
-            "saved": self._serialize_saved_songs()
+            "user": user_data,
+            "likedSongs": saved_songs,
+            "ownedPlaylists": owned_playlists,
+            "followedPlaylists": followed_playlists,
         }
 
-    def _serialize_playlists(self) -> Tuple[List[PlaylistState],
-                                            List[SpotifyID]]:
+    def _serialize_profile(self) -> JSONData:
+        user = self.spotify.current_user()
+        data = user_model_to_json(user)
+        click.secho("Gathered data for user profile", dim=True)
+        return data
+
+    def _serialize_saved_songs(self) -> List[JSONData]:
+        paging = self.spotify.saved_tracks()
+        saved_track_iterator: Generator[tekore.model.SavedTrack, None, None]
+        saved_track_iterator = self.spotify.all_items(paging)  # type: ignore
+
+        click.secho("Gathering data for saved songs...", dim=True)
+        iterator: Iterator[tekore.model.SavedTrack]
+        with click.progressbar(saved_track_iterator) as iterator:
+            data = [track_model_to_json(saved_track)
+                    for saved_track in iterator]
+        click.secho("Gathered data for saved songs", dim=True)
+
+        return data
+
+    def _serialize_playlists(self) -> Tuple[List[JSONData],
+                                            List[JSONData]]:
+        """
+        Return a 2-tuple with data for the owned playlists and followed
+        playlists.
+
+        NOTE: Both playlist types are fetched in one subroutine because
+        the API only supports returning all playlists -- both owned and
+        followed -- instead of individually, so this way we only make
+        one API call.
+        """
         user_id = self.spotify.current_user().id
-        user_playlists = self.spotify.playlists(user_id)
-        simple_playlist_iterator: Generator[SimplePlaylist, None, None] = \
-            self.spotify.all_items(user_playlists)  # type: ignore
 
-        owned: List[PlaylistState] = []
-        followed: List[SpotifyID] = []
+        paging = self.spotify.playlists(user_id)
+        playlist_iterator: Generator[tekore.model.SimplePlaylist, None, None]
+        playlist_iterator = self.spotify.all_items(paging)  # type: ignore
 
-        for simple_playlist in simple_playlist_iterator:
-            owner_id = simple_playlist.owner.id
-            if owner_id == user_id:
-                full_playlist: FullPlaylist = \
-                    self.spotify.playlist(simple_playlist.id)  # type: ignore
-                playlist_state = self._convert_playlist_model(full_playlist)
-                owned.append(playlist_state)
-            else:
-                followed.append(simple_playlist.id)
+        owned: List[JSONData] = []
+        followed: List[JSONData] = []
+
+        click.secho("Gathering data for playlists...", dim=True)
+        iterator: Iterator[tekore.model.SimplePlaylist]
+        with click.progressbar(playlist_iterator) as iterator:
+            for simple_playlist in iterator:
+                data = playlist_model_to_json(simple_playlist, self.spotify)
+                owner_id = simple_playlist.owner.id
+                if owner_id == user_id:
+                    owned.append(data)
+                else:
+                    # Extend data with information about the owner
+                    data["owner"] = user_model_to_json(simple_playlist.owner)
+                    followed.append(data)
+        click.secho("Gathered data for playlists", dim=True)
 
         return (owned, followed)
-
-    def _serialize_saved_songs(self) -> List[SpotifyID]:
-        saved_track_iterator: Generator[SavedTrack, None, None] = \
-            self.spotify.all_items(self.spotify.saved_tracks())  # type: ignore
-
-        return [saved_track.track.id for saved_track in saved_track_iterator]
-
-    def _convert_playlist_model(self, playlist: FullPlaylist) -> PlaylistState:
-        image_url = playlist.images[0].url if playlist.images else None
-
-        playlist_track_iterator: Generator[PlaylistTrack, None, None] = \
-            self.spotify.all_items(playlist.tracks)  # type: ignore
-
-        track_ids = []
-        for playlist_track in playlist_track_iterator:
-            track = playlist_track.track
-            # No support for local tracks yet.  A LocalPlaylistTrack object
-            # has id=None, and that's just annoying.
-            if track is None or isinstance(track, LocalPlaylistTrack):
-                raise Exception(
-                    "Unknown playlist track format. "
-                    "Local tracks are not yet supported."
-                )
-            track_ids.append(track.id)
-
-        return PlaylistState(
-            id=playlist.id,
-            name=playlist.name,
-            description=playlist.description,
-            photo=image_url,
-            tracks=track_ids
-        )
 
 
 @click.command("serialize")
 @click.option("-o", "--output",
               required=True,
-              type=click.File(mode="wb", encoding="utf-8"))
-@click.option("-j", "--with-json", is_flag=True)
-def serialize_command(output: BinaryIO,
-                      with_json: bool,
-                      ) -> None:
+              type=click.File(mode="wt", encoding="utf-8"))
+def serialize_command(output: TextIO) -> None:
     spotify = get_client()
-
-    output_is_json = (Path(output.name).suffix.lower() == ".json")
-    format_choice = "binary"
-    if output_is_json:
-        format_choice = "JSON"
-    elif with_json:
-        format_choice = "binary + JSON"
-    click.secho(f"Serializing your library ({format_choice})...",
-                fg="green")
-
-    library_json = Serializer(spotify).get_library_json()
-    as_bytes = json.dumps(library_json).encode("utf-8")
-
-    # Directly write to the output stream with JSON data as bytes.
-    # Notice with_json is ignored if the output path is already JSON.
-    if output_is_json:
-        output.write(as_bytes)
-        click.secho(f"Saved your library as a JSON document at {output.name}",
-                    fg="green")
-        return
-
-    # Otherwise, convert JSON data into compressed bytes
-    compressed = zlib.compress(as_bytes)
-    output.write(compressed)
-    click.secho(f"Serialized your library into binary file {output.name}",
-                fg="green")
-
-    # If the user wants to keep the JSON data alongside the binary
-    if with_json:
-        # Create it in the same directory and replace the extension (if
-        # exists) with .json.
-        dir_path = Path(output.name).parent
-        output_stem = Path(output.name).stem
-        json_path = dir_path / Path(output_stem + ".json")
-
-        if json_path.exists():
-            click.secho(f"Cannot create a JSON copy at {json_path} because it "
-                        f"already exists. Binary file {output.name} is fine.",
-                        fg="red",
-                        err=True)
-            raise click.Abort
-        with json_path.open("wt", encoding="utf-8") as fp:
-            json.dump(library_json, fp)
-        click.secho(f"Also generated an uncompressed JSON copy at {json_path}",
-                    fg="green")
+    click.secho(f"Serializing your library to JSON...", fg="green")
+    library_json = Serializer(spotify).serialize()
+    json.dump(library_json, output)
+    click.secho(f"Saved your library at {output.name}", fg="green")
