ff8e73bb7fecbf3d460ec76842aeb86071e5c75b
Vincent Lin <vinlin24@outlook.com>
2023-08-12 22:56:27 -0700

    Clean up login command and use new get_client()

--- src/spotify_serialize/commands/login.py
+++ src/spotify_serialize/commands/login.py
@@ -5,11 +5,9 @@ Implement authentication for this application.
 
 import click
 import tekore
-from tekore import RefreshingToken
-from tekore.model import PrivateUser
 
-from ..utils import (APP_SCOPES, CLIENT_ID, CONFIG_DIR, CREDS_PATH,
-                     REDIRECT_URI, get_client, log_event, update_creds)
+from ..client import (APP_SCOPES, CLIENT_ID, CREDS_PATH, REDIRECT_URI,
+                      get_client, update_creds)
 
 REDIRECT_NOTE = (
     f"NOTE: You will be redirected to {REDIRECT_URI} after authorizing "
@@ -17,21 +15,10 @@ REDIRECT_NOTE = (
     "console after the redirect."
 )
 
-AUTH_LOG_PATH = CONFIG_DIR / "auth.log"
-
 
 def prompt_confirmation() -> None:
     click.secho(REDIRECT_NOTE, fg="yellow")
     click.confirm("Login through Spotify?", abort=True, show_default=True)
-
-
-def write_to_log(token: RefreshingToken, user: PrivateUser) -> None:
-    body = (
-        f"Logged in as user {user.display_name} (ID: {user.id})\n"
-        f"Saved access_token={token.access_token!r} to {CREDS_PATH}\n"
-        f"Saved refresh_token={token.refresh_token!r} to {CREDS_PATH}"
-    )
-    log_event(AUTH_LOG_PATH, body)
 
 
 @click.command("login")
@@ -48,4 +35,3 @@ def login_command() -> None:
         f"Wrote access tokens to {CREDS_PATH}"
     )
     click.secho(notice, fg="green")
-    write_to_log(token, user)
