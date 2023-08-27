02f95696b7e709af0803da8601883eb08d4e2db6
Vincent Lin <vinlin24@outlook.com>
2023-08-26 20:02:17 -0700

    Add *.snapshot.zip to files pruned by clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 find . -type d -name "*.snapshot" -not -path ".venv" -prune -exec rm -rf {} \;
+find . -type d -name "*.snapshot.zip" -not -path ".venv" -prune -exec rm -rf {} \;
 find . -type d -name "__pycache__" -not -path ".venv" -prune -exec rm -rf {} \;
